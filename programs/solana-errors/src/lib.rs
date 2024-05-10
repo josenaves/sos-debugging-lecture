72bed5fb4aaeec4b13a61ea3c438663d021a1359
José Naves Moura Neto <josenaves@gmail.com>
2024-05-10 19:59:51 +0200

    Fix issues

--- programs/solana-errors/src/lib.rs
+++ programs/solana-errors/src/lib.rs
@@ -7,11 +7,13 @@ declare_id!("Gp3jcr7dqCcgp3QbQdcwjS5p5n5usRLoxesQuNaHm4GD");
 pub mod solana_errors {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let data = &mut ctx.accounts.data;
+    pub fn initialize(ctx: Context<Initialize>, count: u8) -> Result<()> {
+        let data: &mut Account<MyData> = &mut ctx.accounts.data;
+
+        require!(count <= 10, MyError::InvalidCount);
 
         data.authority = ctx.accounts.user.key();
-        data.counter = 0;
+        data.counter = math_function(count).unwrap();
 
         msg!("data.conter = {}", data.counter);
         msg!("data pubkey = {}", data.key().to_string());
@@ -21,14 +23,41 @@ pub mod solana_errors {
     }
 }
 
+fn math_function(count: u8) -> Option<u8> {
+    10u8.checked_sub(count)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_small_number_success() {
+        assert_eq!(math_function(2), Some(8));
+    }
+
+    #[test]
+    fn test_limit_number_success() {
+        assert_eq!(math_function(10), Some(0));
+    }
+
+    #[test]
+    fn test_big_number_fail() {
+        assert_eq!(math_function(11), None);
+    }
+}
+
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
     user: Signer<'info>,
 
     #[account(init,
-        space = 32 + 1,
+        space = 8 + 32 + 1,  //  8 bytes for DESCRIMINANT + 32 bytes for the PubKey + 1 byte for counter
         payer = user,
+        seeds = [b"data"],
+        bump
     )]
     data: Account<'info, MyData>,
 
@@ -39,4 +68,10 @@ pub struct Initialize<'info> {
 pub struct MyData {
     authority: Pubkey,
     counter: u8,
+}
+
+#[error_code]
+pub enum MyError {
+    #[msg("Invalid count value")]
+    InvalidCount
 }
--- tests/solana-errors.ts
+++ tests/solana-errors.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaErrors } from "../target/types/solana_errors";
 import { Keypair, PublicKey, LAMPORTS_PER_SOL, SystemProgram, sendAndConfirmTransaction } from '@solana/web3.js';
+import { assert } from "chai";
 
 describe("solana-errors", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -9,8 +10,7 @@ describe("solana-errors", () => {
 
   const program = anchor.workspace.SolanaErrors as Program<SolanaErrors>;
   const user = Keypair.generate();
-  const data = Keypair.generate();
-  const data2 = Keypair.generate();
+  const [data] = PublicKey.findProgramAddressSync([Buffer.from("data")], program.programId);
 
   before("prepare", async () => {
     await airdrop(anchor.getProvider().connection, user.publicKey)
@@ -19,30 +19,46 @@ describe("solana-errors", () => {
   it("Is initialized!", async () => {
 
     const tx = await program.methods
-      .initialize()
+      .initialize(10)
       .accountsStrict({
         user: user.publicKey,
-        data: data.publicKey,
+        data: data,
         systemProgram: SystemProgram.programId
       })
-      .signers([user, data])
-      .rpc();
+      .signers([user])
+      .rpc({ commitment: "confirmed" });
 
-    console.log("Your transaction signature", tx);
-    console.log("data account pubkey: ", data.publicKey.toString())
-
-    let t = await program.methods
-      .initialize()
-      .accountsStrict({
-        user: user.publicKey,
-        data: data2.publicKey,
-        systemProgram: SystemProgram.programId
-      })
-      .signers([user, data2])
-      .rpc();
+    let dataAccount = await program.account.myData.fetch(data);
+    assert.deepEqual(dataAccount.authority, user.publicKey);
+    assert.strictEqual(dataAccount.counter, 0);
 
   });
+
+  it("Cannot initialize with incorrect data account!", async () => {
+
+    const bad_data_account = Keypair.generate(); // not a PDA as expected
+
+    try {
+      const tx = await program.methods
+      .initialize(1)
+      .accountsStrict({
+        user: user.publicKey,
+        data: bad_data_account.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([user])
+      .rpc();
+      assert.fail();  // always fail if the instruction did not fail as expected!
+
+    } catch(_err) {
+      const err = anchor.AnchorError.parse(_err.logs);
+      assert.strictEqual(err.error.errorCode.code, "ConstraintSeeds");
+    }
+  });
+
 });
+
+
 
 
 async function airdrop(connection: any, address: any, amount = 1000000000) {
